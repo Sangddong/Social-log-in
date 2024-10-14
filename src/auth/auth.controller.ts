f4d3ce7657ca1d50970be0772a61ab046095d17f
sangddong <hehe74029@gmail.com>
2024-10-15 03:04:01 +0900

    feat: 인가 코드 받기

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Get(':platform')
+  getSocialCode(
+    @Param('platform') platform: string,
+    @Res() response: Response,
+  ) {
+    const url = this.authService.getSocialSignInCode(platform);
+
+    response.redirect(url);
+  }
+
+  @Get(':platform/callback')
+  async socialCallback(
+    @Param('platform') platform: string,
+    @Query('code') code: string,
+    @Res() response: Response,
+  ) {
+    console.log(code);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Param } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import axios from 'axios';
+import { platform } from 'os';
 import { User } from 'src/users/users.entity';
 import { UsersService } from 'src/users/users.service';
 import { Repository } from 'typeorm';
@@ -11,4 +13,46 @@ export class AuthService {
     private usersRepository: Repository<User>,
     private readonly usersService: UsersService,
   ) {}
+
+  getSocialSignInCode(platform: string) {
+    const baseUrl = this.getSocialBaseUrl(platform);
+    const params = this.getSocialParams(platform);
+
+    return `${baseUrl}?${new URLSearchParams(params)}`;
+  }
+
+  private getSocialBaseUrl(platform: string) {
+    const baseUrls = {
+      naver: 'https://nid.naver.com/oauth2.0/authorize',
+      google: 'https://accounts.google.com/o/oauth2/v2/auth',
+      kakao: 'https://kauth.kakao.com/oauth/authorize',
+    };
+
+    return baseUrls[platform];
+  }
+
+  private getSocialParams(platform: string) {
+    const queryParams = {
+      naver: {
+        response_type: 'code',
+        client_id: process.env.N_CLIENT_ID,
+        redirect_uri: process.env.N_REDIRECT_URI,
+        state: process.env.N_STATE,
+      },
+      google: {
+        response_type: 'code',
+        client_id: process.env.G_CLIENT_ID,
+        redirect_uri: process.env.G_REDIRECT_URI,
+        scope: `https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email`,
+        access_type: 'offline',
+      },
+      kakao: {
+        response_type: 'code',
+        client_id: process.env.K_REST_API_KEY,
+        redirect_uri: process.env.K_REDIRECT_URI,
+      },
+    };
+
+    return queryParams[platform];
+  }
 }
